a4f8ad6890c7cf88de23c4f17aa2b980cb857133
john32313 <francois.jonathan13@gmail.com>
2021-04-22 11:50:00 +0200

    made quest discover apollo and setup apollo cient with react/ts

---
+++ src/appoloClient.ts
@@ -0,0 +1,12 @@
+import {
+  ApolloClient,
+  InMemoryCache,
+  NormalizedCacheObject,
+} from "@apollo/client";
+
+const client: ApolloClient<NormalizedCacheObject> = new ApolloClient({
+  uri: "https://api.spacex.land/graphql/",
+  cache: new InMemoryCache(),
+});
+
+export default client;
